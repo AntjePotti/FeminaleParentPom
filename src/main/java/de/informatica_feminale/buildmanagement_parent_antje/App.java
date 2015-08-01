08b33031d26c50b25510207f7a7bc46dae985d8d
AntjePotti <antje_pottschmidt@gmx.net>
2015-08-01 09:22:22 +0200

    first commit

---
+++ src/main/java/de/informatica_feminale/buildmanagement_parent_antje/App.java
@@ -0,0 +1,13 @@
+package de.informatica_feminale.buildmanagement_parent_antje;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
